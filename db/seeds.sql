69d99cb16e3f7deffeac521579c74f7b18747880
Fluent85 <tshabalala.richard@gmail.com>
2020-01-03 17:05:39 -0600

    created 1st version of server.js file, burger db, seeds file, and schema file

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Chesse burger');
+INSERT INTO burgers (name) VALUES ('Bacon burger');
+-- INSERT INTO burgers (name, sleepy) VALUES ('Sylvester', true);
+-- INSERT INTO burgers (name, sleepy) VALUES ('Marilyn Manson', true);
+-- INSERT INTO burgers (name, sleepy) VALUES ('Joe Biden', true);
+-- INSERT INTO burgers (name) VALUES ('Owens');
