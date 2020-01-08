1098a6727837049f0eb21430d6c873204120160a
Fluent85 <tshabalala.richard@gmail.com>
2020-01-08 16:03:52 -0600

    added add a burger feature

--- config/orm.js
+++ config/orm.js
@@ -18,7 +18,40 @@ var orm = {
             }
             cb(result);
         });
-    }
-}
+    },
+    create: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+    
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      },
+      delete: function(table, condition, cb) {
+        var queryString = "DELETE FROM " + table;
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      }
+    };
 
 module.exports = orm;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,8 @@
 INSERT INTO burgers (burger_name) VALUES ('Chesse burger');
 INSERT INTO burgers (burger_name) VALUES ('Bacon burger');
--- INSERT INTO burgers (name, sleepy) VALUES ('Sylvester', true);
--- INSERT INTO burgers (name, sleepy) VALUES ('Marilyn Manson', true);
--- INSERT INTO burgers (name, sleepy) VALUES ('Joe Biden', true);
--- INSERT INTO burgers (name) VALUES ('Owens');
+INSERT INTO burgers (burger_name) VALUES ('Ham burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie burger');
+-- INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
+-- INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
+-- INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
+-- INSERT INTO cats (name) VALUES ('Owens');
